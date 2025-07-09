978fd2164fa798a542ef63989c31ec18d3af9c96
SankalpPimpalkar <pimpalkarsankalp8@gmail.com>
2025-07-10 01:02:08 +0530

    Add initial HTML structure and Vite logo SVG to the distribution folder

--- app/__init__.py
+++ app/__init__.py
@@ -1,27 +1,28 @@
-from flask import Flask, jsonify
+from flask import Flask, send_from_directory
 from app.extensions import dbconnect,mongo
 from flask_cors import CORS
+import os
 
 # Creating our flask app
 def create_app():
 
-    app = Flask(__name__)
+    app = Flask(__name__, static_folder='dist/assets', template_folder='dist')
     # Configuration
     CORS(app=app)
 
     # Database connection
     dbconnect(app=app)
 
-    @app.route('/', methods=['GET'])
-    def home():
-        print(mongo.db)
-        return jsonify({
-            "success": True,
-            "message": "App is running",
-            "payload": {
-                "mongodb": mongo.db.name,
-            }
-        })
+    print("STATIC FOLDER:", app.static_folder)
+
+    @app.route('/', defaults={'path': ''})
+    @app.route('/<path:path>')
+    def serve(path):
+        print("SERVING")
+        full_path = os.path.join(app.static_folder, path)
+        if path and os.path.exists(full_path):
+            return send_from_directory(app.static_folder, path)
+        return send_from_directory(app.template_folder, 'index.html')
     
     # registering all the blueprints
     from app.webhook.routes import webhook
